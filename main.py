c3677fd8792ada402daea5a9257800995899c7e3
clib <vitor.carvalho.ufu@gmail.com>
2023-12-19 19:04:06 -0300

    Adding and Fixing Error Checkings

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ import pyperclip
 import time
 from utils import *
 
+# SETTING STANDARD CONFIDENCE TO FIND IMAGES
+STANDARD_CONFIDENCE = 0.8
+# SETTING STANDARD DELAY TO WAIT FOR THE COMPUTER TO CATCH UP
+STANDARD_DELAY = 1
+
 STANDARD_PADDING = 10
 STANDARD_X_PADD_MULTIPLIER = 5
 WINDOW_WIDTH = 800
@@ -44,6 +49,9 @@ class Interface(tk.Tk):
         self.sheet_chooser = ttk.Combobox(self, state='readonly')
         self.range_txt_var = tk.StringVar()
         self.range_label = ttk.Label(self, text='Range')
+        self.delay_txt_var = tk.StringVar()
+        self.delay_label = ttk.Label(self, text='Delay (in seconds)')
+        self.delay_text = tk.Entry(self, textvariable=self.delay_txt_var)
         self.range_text = tk.Entry(self, textvariable=self.range_txt_var)
         self.message_label = ttk.Label(self, text='Message')
         self.message_text = tk.Text(self, height=5, width=50)
@@ -57,10 +65,12 @@ class Interface(tk.Tk):
         self.sheet_chooser.grid(row=1, column=1, padx=STANDARD_PADDING*STANDARD_X_PADD_MULTIPLIER, pady=STANDARD_PADDING, sticky='w')
         self.range_label.grid(row=2, column=0, padx=STANDARD_PADDING*STANDARD_X_PADD_MULTIPLIER, pady=STANDARD_PADDING)
         self.range_text.grid(row=2, column=1, padx=STANDARD_PADDING*STANDARD_X_PADD_MULTIPLIER, pady=STANDARD_PADDING, sticky='w')
-        self.message_label.grid(row=3, column=0, padx=STANDARD_PADDING*STANDARD_X_PADD_MULTIPLIER, pady=STANDARD_PADDING)
-        self.message_text.grid(row=3, column=1, padx=STANDARD_PADDING*STANDARD_X_PADD_MULTIPLIER, pady=STANDARD_PADDING, sticky='w')
-        self.send_messages_btn.grid(row=4, column=0, padx=STANDARD_PADDING, pady=STANDARD_PADDING, columnspan=2)
-        self.stop_messages_btn.grid(row=5, column=0, padx=STANDARD_PADDING, pady=STANDARD_PADDING, columnspan=2)
+        self.delay_label.grid(row=3, column=0, padx=STANDARD_PADDING*STANDARD_X_PADD_MULTIPLIER, pady=STANDARD_PADDING)
+        self.delay_text.grid(row=3, column=1, padx=STANDARD_PADDING*STANDARD_X_PADD_MULTIPLIER, pady=STANDARD_PADDING, sticky='w')
+        self.message_label.grid(row=4, column=0, padx=STANDARD_PADDING*STANDARD_X_PADD_MULTIPLIER, pady=STANDARD_PADDING)
+        self.message_text.grid(row=4, column=1, padx=STANDARD_PADDING*STANDARD_X_PADD_MULTIPLIER, pady=STANDARD_PADDING, sticky='w')
+        self.send_messages_btn.grid(row=5, column=0, padx=STANDARD_PADDING, pady=STANDARD_PADDING, columnspan=2)
+        self.stop_messages_btn.grid(row=6, column=0, padx=STANDARD_PADDING, pady=STANDARD_PADDING, columnspan=2)
         
         self.setup_interface()
 
@@ -70,6 +80,7 @@ class Interface(tk.Tk):
         self.sheet_chooser.config(values=[0])
         self.sheet_chooser.current(0)
         self.range_txt_var.set("D:D")
+        self.delay_txt_var.set("60")
         message_txt = """Hi, I just saw your ad on craigslist. I can get you more customers. Please give me a call. Thanks!"""
         self.message_text.insert(tk.END, message_txt)
         self.send_messages_btn.config(text='Send Messages', state='disabled')
@@ -84,6 +95,7 @@ class Interface(tk.Tk):
 
     def get_sheets(self, file_path):
         app = xlw.App(visible=False)
+        app.display_alerts = False
         wb = app.books.open(file_path)
         sheets = wb.sheets
         sheet_number = []
@@ -100,7 +112,13 @@ class Interface(tk.Tk):
         path = self.file_path.get()
         sheet_name = int(self.sheet_chooser.get())
         range_name = self.range_text.get()
-        print("Range name: " + range_name)
+        try:
+            delay = int(self.delay_text.get())
+            if delay < 0:
+                messagebox.showinfo("Information","Delay must be a positive integer!")
+        except:
+            messagebox.showinfo("Information","Delay must be a positive integer!")
+            return
         message = self.message_text.get("1.0", tk.END)
         # Getting the last phone number used
         data = fetchLastRow(path, sheet_name)
@@ -111,15 +129,15 @@ class Interface(tk.Tk):
             if answer == False:
                 return
             self.delete_laststopbk_file(path, sheet_name)
-        self.start_thread(path, sheet_name, range_name, message)
+        self.start_thread(path, sheet_name, range_name, message, delay)
 
     def starting_script_message_box_thread(self):
         new_thread = Thread(target=messagebox.showinfo, args=("Information","Starting the script..."))
         new_thread.start()
 
     # Start new Thread (Make the GUI responsive)
-    def start_thread(self, path:str, sheet_name: int, range_name:str, message:str):
-        new_thread = Thread(target=self.sendMessages, args=(path, sheet_name, range_name, message))
+    def start_thread(self, path, sheet_name, range_name, message, delay):
+        new_thread = Thread(target=self.send_messages, args=(path, sheet_name, range_name, message, delay))
         new_thread.start()
 
     def stop_script(self):
@@ -131,13 +149,54 @@ class Interface(tk.Tk):
         file_name = path.split("\\")[-1].split('.')[0] + "-" + str(sheet_name) + ".txt"
         file_path = os.getcwd() + "\\src\\laststopbk\\" + file_name
         os.remove(file_path)
-        
-    def sendMessages(self, path:str, sheet_name: int, range_name:str, message:str):
-        phone_numbers = fetchPhoneNumbers(path, sheet_name, range_name)
-        phone_numbers = parsePhoneNumbers(phone_numbers)
 
+    def fetch_phone_numbers(self, path, sheet_name, range_name):
+        # Read CSV File
+        app = xlw.App(visible=False)
+        app.display_alerts = False
+        wb = app.books.open(path)
+        # Select Sheet
+        sheet = wb.sheets[sheet_name]
+        # Select Range
+        try:
+            range = sheet.range(range_name)
+            # Fetch Values
+            values = range.value
+            # Close CSV File
+            wb.save(path)
+            app.kill()
+            # Return Values
+            return values
+        except:
+            print("Information","The provided Range in invalid!")
+            return None
+        
+    def parse_phone_numbers(self, phone_numbers):
         if phone_numbers is None:
-            print("Phone numbers could not be extracted successfully. Please Try again!")
+            print("No Phone Numbers, unable to parse...")
+            return None
+        # Remove empty values
+        phone_numbers = list(filter(lambda item: item is not None and "-" in item and not any(c.isalpha() for c in item), phone_numbers))
+        # Split on space
+        spaced_phone_numbers = list(filter(lambda item: " " in item, phone_numbers))
+        # Remove spaces
+        phone_numbers = list(filter(lambda item: " " not in item, phone_numbers))
+        # Treating multiple phone numbers in one cell
+        for item in spaced_phone_numbers:
+            item = item.split(" ")
+            for sub_item in item:
+                phone_numbers.append(sub_item)
+        # Remove duplicates
+        phone_numbers = list(dict.fromkeys(phone_numbers))
+        
+        return phone_numbers
+        
+    def send_messages(self, path, sheet_name, range_name, message, delay):
+        phone_numbers = self.fetch_phone_numbers(path, sheet_name, range_name)
+        phone_numbers = self.parse_phone_numbers(phone_numbers)
+
+        if phone_numbers is None or len(phone_numbers)==0:
+            messagebox.showinfo("Information","Phone numbers could not be extracted successfully. Please check the given Range and Try again!")
             return
         
         ##  THIS SECTION IS COMMENTED BECAUSE YOUR DESKTOP DOES NOT SHOW THE LINE2 APP ICON
@@ -145,19 +204,25 @@ class Interface(tk.Tk):
         ##  AND MAKE SURE THE OPEN APPS ICONS ARE SHOWING ON YOUR TASKBAR
 
         # Look for Line2 Desktop Icon -- DOING IT ONLY ONCE IS ENOUGH
-        app_desktop_icon = pyautogui.locateOnScreen(os.getcwd() + LINE2_DESKTOP_IMG, confidence=STANDARD_CONFIDENCE)
-        if app_desktop_icon is None:
+        try:
+            app_desktop_icon = pyautogui.locateOnScreen(os.getcwd() + LINE2_DESKTOP_IMG, confidence=STANDARD_CONFIDENCE)
+        except:
             messagebox.showinfo("Information","Line2 Desktop Icon not found. Please open App.")
             return
-        pyautogui.click(app_desktop_icon)
+        pyautogui.moveTo(app_desktop_icon)
+        time.sleep(STANDARD_DELAY)
+        pyautogui.move(0, -20, 1)
+        pyautogui.click()
         time.sleep(STANDARD_DELAY)
 
         # Look for messages Icon (UNSELECTED) -- DOING IT ONLY ONCE IS ENOUGH
-        messages_icon = pyautogui.locateOnScreen(os.getcwd() + LINE2_MESSAGES_IMG, confidence=STANDARD_CONFIDENCE)
-        if messages_icon is None:
+        try:
+            messages_icon = pyautogui.locateOnScreen(os.getcwd() + LINE2_MESSAGES_IMG, confidence=STANDARD_CONFIDENCE)
+        except:
             # Look for messages Icon (SELECTED/BLUE)
-            messages_icon = pyautogui.locateOnScreen(os.getcwd() + LINE2_BLUE_MESSAGES_IMG, confidence=STANDARD_CONFIDENCE)
-            if messages_icon is None:
+            try:
+                messages_icon = pyautogui.locateOnScreen(os.getcwd() + LINE2_BLUE_MESSAGES_IMG, confidence=STANDARD_CONFIDENCE)
+            except:
                 messagebox.showinfo("Information","Line2 messages Icon not found. Please try again!")
                 return
         pyautogui.click(messages_icon)
@@ -175,22 +240,26 @@ class Interface(tk.Tk):
                 return
             return_status = self.send_message(phone_number, message)
             if return_status is None:
-                print("Failed to send message to: " + phone_number)
+                messagebox.showinfo("Information","Failed to send message to: " + phone_number + ". Please restart the script!")
                 # IF SENDING A MESSAGE TO A NUMBER FAILS, THE WHOLE SCRIPT WILL STOP, IF YOU WOULD LIKE IT TO CONTINUE
                 # SENDING MESSAGES DESPITE THE FAILURES, COMMENT THE RETURN STATEMENT BELOW
                 return
             else:
                 print("Sent message to: " + phone_number)
                 saveLastRow(path, sheet_name, last_row + index, phone_number, len(phone_numbers) - 1)
-                # Waiting for 5 seconds
-                time.sleep(STANDARD_DELAY*5)
+                # Waiting for 60 seconds
+                i = 0
+                while(i < delay and not self.event.is_set()):
+                    i += 1
+                    time.sleep(STANDARD_DELAY)
 
         messagebox.showinfo("Information","All messages have been sent successfully!")
 
 
     def send_message(self, phone_number, message):
-        new_message_icon = pyautogui.locateOnScreen(os.getcwd() + LINE2_NEW_MESSAGE_IMG, confidence=STANDARD_CONFIDENCE)
-        if new_message_icon is None:
+        try:
+            new_message_icon = pyautogui.locateOnScreen(os.getcwd() + LINE2_NEW_MESSAGE_IMG, confidence=STANDARD_CONFIDENCE)
+        except:
             messagebox.showinfo("Information","New message Icon not found. Please Try again!")
             return
         pyautogui.click(new_message_icon)
--- utils.py
+++ utils.py
@@ -2,45 +2,6 @@ import os.path
 from src import *
 import xlwings as xlw
 
-# SETTING STANDARD CONFIDENCE TO FIND IMAGES
-STANDARD_CONFIDENCE = 0.7
-# SETTING STANDARD DELAY TO WAIT FOR THE COMPUTER TO CATCH UP
-STANDARD_DELAY = 1
-
-def fetchPhoneNumbers(path:str, sheet_name: int, range_name:str) -> list:
-    # Read CSV File
-    app = xlw.App(visible=False)
-    wb = app.books.open(path)
-    # Select Sheet
-    sheet = wb.sheets[sheet_name]
-    # Select Range
-    range = sheet.range(range_name)
-    # Fetch Values
-    values = range.value
-    # Close CSV File
-    wb.save(path)
-    app.kill()
-    # Return Values
-    return values
-
-
-def parsePhoneNumbers(phone_numbers:list) -> list:
-    # Remove empty values
-    phone_numbers = list(filter(lambda item: item is not None and "-" in item, phone_numbers))
-    # Split on space
-    spaced_phone_numbers = list(filter(lambda item: " " in item, phone_numbers))
-    # Remove spaces
-    phone_numbers = list(filter(lambda item: " " not in item, phone_numbers))
-    # Treating multiple phone numbers in one cell
-    for item in spaced_phone_numbers:
-        item = item.split(" ")
-        for sub_item in item:
-            phone_numbers.append(sub_item)
-    # Remove duplicates
-    phone_numbers = list(dict.fromkeys(phone_numbers))
-    
-    return phone_numbers
-
 
 def saveLastRow(path:str, sheet_name: int, index:int, phone_number:str, last_index:int) -> None:
     path = path.replace("/", "\\")
